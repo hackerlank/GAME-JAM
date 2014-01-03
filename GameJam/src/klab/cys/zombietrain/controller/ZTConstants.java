742ea956978705e6af4742a713b3a24a820cc6f7
ranny <francisroda@hotmail.com>
2014-01-03 21:59:31 +0800

    Move control contants.

---
+++ GameJam/src/klab/cys/zombietrain/controller/ZTConstants.java
@@ -0,0 +1,11 @@
+package klab.cys.zombietrain.controller;
+
+public class ZTConstants {
+	public static final int MOVE_UP = 0;
+	public static final int MOVE_LEFT = 1;
+	public static final int MOVE_DOWN = 2;
+	public static final int MOVE_RIGHT = 3;
+
+	public static final int MIN_MOVE_SPACE = 1;
+	public static final int MAX_MOVE_SPACE = 8;
+}
