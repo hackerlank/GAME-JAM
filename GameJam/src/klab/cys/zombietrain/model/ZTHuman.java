55d0a535d5646cdf270e38a712371cdefa23cdbb
ranny <francisroda@hotmail.com>
2014-01-04 10:58:08 +0800

    Added new AI movement to human factory.

--- GameJam/src/klab/cys/zombietrain/model/ZTHuman.java
+++ GameJam/src/klab/cys/zombietrain/model/ZTHuman.java
@@ -1,18 +1,26 @@
 package klab.cys.zombietrain.model;
 
+import klab.cys.zombietrain.ai.ZTAIBulletMoveDown;
+import klab.cys.zombietrain.ai.ZTAIBulletMoveLeft;
+import klab.cys.zombietrain.ai.ZTAIBulletMoveRight;
+import klab.cys.zombietrain.ai.ZTAIBulletMoveUp;
 import klab.cys.zombietrain.ai.ZTAIMove;
 import klab.cys.zombietrain.ai.ZTAIMoveLeftRight;
 import klab.cys.zombietrain.ai.ZTAIMoveRoaming;
+import klab.cys.zombietrain.ai.ZTAIMoveRoamingFiring;
 import klab.cys.zombietrain.ai.ZTAIMoveStay;
 import klab.cys.zombietrain.ai.ZTAIMoveUpDown;
-import klab.cys.zombietrain.controller.ZTConstants;
-import klab.cys.zombietrain.view.ZTGameScreen;
 
 public class ZTHuman {
 	public static final int MOVEMENT_STAY = 0;
 	public static final int MOVEMENT_UP_DOWN = 1;
 	public static final int MOVEMENT_LEFT_RIGHT = 2;
 	public static final int MOVEMENT_ROAM = 3;
+	public static final int MOVEMENT_ROAM_FIRING = 4;
+	public static final int MOVEMENT_UP = 10;
+	public static final int MOVEMENT_DOWN = 11;
+	public static final int MOVEMENT_LEFT = 12;
+	public static final int MOVEMENT_RIGHT = 13;
 	public int x, y;
 	public int type;
 	
@@ -38,6 +46,16 @@ public class ZTHuman {
 			ai = new ZTAIMoveLeftRight(this, moveSpace);
 		} else if(type == MOVEMENT_ROAM){
 			ai = new ZTAIMoveRoaming(this, moveSpace);
+		} else if(type == MOVEMENT_ROAM_FIRING){
+			ai = new ZTAIMoveRoamingFiring(this, moveSpace, 5);
+		} else if(type == MOVEMENT_UP){
+			ai = new ZTAIBulletMoveUp(this, moveSpace);
+		} else if(type == MOVEMENT_DOWN){
+			ai = new ZTAIBulletMoveDown(this, moveSpace);
+		} else if(type == MOVEMENT_LEFT){
+			ai = new ZTAIBulletMoveLeft(this, moveSpace);
+		} else if(type == MOVEMENT_RIGHT){
+			ai = new ZTAIBulletMoveRight(this, moveSpace);
 		}
 		
 		ai.findMoveLocation();
